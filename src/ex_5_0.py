4635d39e62368b901eb97fde9a631a4dfa309bd7
yedukondaluM <156935974+yedukondaluM@users.noreply.github.com>
2024-02-28 13:58:16 -0600

    merege

--- src/ex_5_0.py
+++ src/ex_5_0.py
@@ -2,9 +2,20 @@
 
 
 def line_count(infile):
-    def line_count(infile):
-        with open(infile, "r") as f:
-            print(len(f.readlines()))
+    with open(infile, "r") as f:
+        print(len(f.readlines()))
+
+
+if __name__ == "__main__":
+    # get the utility function for path discovery
+    try:
+        from src.util import get_repository_root
+    except ImportError:
+        from util import get_repository_root
+
+    # Test line_count with a file from the data directory
+    data_directory = get_repository_root() / "data"
+    line_count(data_directory / "ex_5_2-data.csv")
 
 
 if __name__ == "__main__":
--- src/ex_5_1.py
+++ src/ex_5_1.py
@@ -24,3 +24,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.infile:
         main(args.infile)
+
